36b041bb706589a5b52c1275e826b8cb926a79fc
Arnas <arnasdickus@protonmail.com>
2022-05-04 23:18:56 +0300

    state logic

--- src/pages/use-state/index.tsx
+++ src/pages/use-state/index.tsx
@@ -1,18 +1,34 @@
-import React from 'react'
-import Layout from '../../components/layout/layout'
-import { IHeadSeo } from '../../shared/interfaces'
+/* eslint-disable react/no-unescaped-entities */
+import Layout from '@components/layout/layout'
+import { IHeadSeo } from '@shared/interfaces'
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components';
 
 const UseStatePage = () => {
+  console.log('hello start');
   const headSeoData: IHeadSeo = {
     title: "Use State Page",
     content: "Use State hooks",
     description: "Training for useState hook"
   }
+  const [increment, setIncrement] = useState(0);
+
   return (
     <Layout headSeoData={headSeoData}>
-      <div>UseStatePage</div>
+      <ContainerUseStatePage>
+        <button onClick={() => {
+          setIncrement(increment + 1);
+          console.log('increment', increment);
+        }}>Increment</button>
+        <button onClick={() => setIncrement(increment - 1)}>Decrement</button>
+        <p>{increment}</p>
+      </ContainerUseStatePage>
     </Layout>
   )
 }
 
-export default UseStatePage
+export default UseStatePage;
+
+const ContainerUseStatePage = styled.div`
+
+`;
